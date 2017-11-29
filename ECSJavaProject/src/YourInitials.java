302204f85c989c0bb1a6282c4f5da91ee930f820
sreza9367 <RezaS9367@sduhsd.lan>
2017-11-29 11:51:28 -0800

    My Intitials

---
+++ ECSJavaProject/src/YourInitials.java
@@ -0,0 +1,29 @@
+
+public class YourInitials 
+{
+	public static void main( String[] args )
+	{ 
+		//The Letter S
+		
+		System.out.println(" SSS ");
+		System.out.println("S   S");
+		System.out.println("S    ");
+		System.out.println(" SSS ");
+		System.out.println("    S");
+		System.out.println("S   S");
+		System.out.println(" SSS ");
+		System.out.println("  ");
+		
+		//The Letter R
+		
+		System.out.println("RRRR ");
+		System.out.println("R   R");
+		System.out.println("R   R");
+		System.out.println("RRRR ");
+		System.out.println("R R  ");
+		System.out.println("R  R ");
+		System.out.println("R   R");
+	}
+
+	
+}
