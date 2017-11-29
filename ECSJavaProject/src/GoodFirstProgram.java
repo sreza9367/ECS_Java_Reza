85a893bd2db98f60f977273a40fde912d881dc97
sreza9367 <RezaS9367@sduhsd.lan>
2017-11-29 11:42:43 -0800

    Good First Program

---
+++ ECSJavaProject/src/GoodFirstProgram.java
@@ -0,0 +1,15 @@
+
+public class GoodFirstProgram {
+	public static void main( String[] args )
+	{
+		System.out.println("Hello World!");
+		System.out.println("Hello again");
+		System.out.println("I like tryping this");
+		System.out.println("This is fun,");
+		System.out.println("Yay! Printing.");
+		System.out.println("I'd much rather you 'not'.");
+		System.out.println("I \"said\" do not touch this.");
+		System.out.println("Please don't.");
+	}
+
+}
