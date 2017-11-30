8a969477b2b088ea771f916ec406acfaec3095d6
sreza9367 <RezaS9367@sduhsd.lan>
2017-11-30 10:08:58 -0800

    Using Variables

---
+++ ECSJavaProject/src/UsingVariables.java
@@ -0,0 +1,19 @@
+
+public class UsingVariables 
+{
+	public static void main(String[] args) 
+	{
+		int first;
+		double second;
+		String third;
+		
+		first = 113;
+		second = 2.7182;
+		third = "Computer Science";
+		
+		System.out.println("This is room " + first);
+		System.out.println("e is close to " + second);
+		System.out.println("I'm learning a bit about " + third);
+		
+	}
+}
