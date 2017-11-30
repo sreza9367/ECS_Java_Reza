d5714d719db2cf572f730f37acf8444d83bb95eb
sreza9367 <RezaS9367@sduhsd.lan>
2017-11-30 10:31:04 -0800

    Still Using Variables

---
+++ ECSJavaProject/src/StillUsingVariables.java
@@ -0,0 +1,20 @@
+
+public class StillUsingVariables 
+{
+	
+	public static void main(String[] args) 
+	{ 
+		String yourName;
+		int yourGradYear;
+		
+		yourName = "Samiha Reza";
+		yourGradYear = 2019;
+		
+		System.out.println("My name is " + yourName + " and I'll graduate in " + yourGradYear + ".");
+		
+		
+	
+		
+	}
+
+}
